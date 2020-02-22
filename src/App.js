89ca5d169d0745d4ac3aa413ff2932f556d0f1ae
e <2182620965@qq.com>
2020-02-22 16:34:49 +0800

    初步实现了画直线的功能

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Drawing from "./component/tool-circle-hook";
+import Line from './component/draw-line-hook'
 import './App.css';
 
 function App() {
 
   return (
     <div>
-        <Drawing />
+        {/*<Drawing />*/}
+        <Line/>
     </div>
   );
 }
