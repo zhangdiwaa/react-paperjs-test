759d73d9f337d728209e86291f104caa3d0e7414
LYM <945002585@qq.com>
2020-02-26 01:29:26 +0800

    `

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 //ts中引入的写法
 import * as React from "react";
+import * as paper from "paper";
 //react中引入的写法
 import { Layout } from 'antd';
-import Sidebar from './UI/Sidebar'
 import SplitPane from 'react-split-pane';
-import { PaperContainer, Circle, Layer } from '@psychobolt/react-paperjs';
 import './App.css';
+import { useEffect } from "react";
+import DrawHandle from "./UI/DrawHandle";
 const { Header, Sider, Content } = Layout;
-const Shapes = () => <Circle center={[120, 50]} radius={35} fillColor="#00FF00" />;
 
+const App=()=>{
+  let myCanvas = null;
+  //此处仿照老师绑定canvas的方法
+  useEffect(() => {
+    paper.install(window);
+    paper.setup(myCanvas)
+  });
 
-function App() {
   return (
     <Layout className="me-layout">
       <Header className="me-header">
@@ -18,15 +24,12 @@ function App() {
       </Header>
       <Layout>
         <Sider width={48} className="me-left-bar">
-          <Sidebar />
+          <DrawHandle></DrawHandle>
         </Sider>
         <Content className="me-canvas">
-          <PaperContainer>
-            <Circle center={[80, 50]} radius={35} fillColor="red" />
-            <Layer>
-              <Shapes />
-            </Layer>
-          </PaperContainer>
+          <canvas 
+          ref={ref=>{myCanvas=ref}}
+          id="myCanvas"></canvas>
         </Content>
         <Content className="me-right-bar">
           <SplitPane
--- src/UI/Sidebar.tsx
+++ src/UI/Sidebar.tsx
@@ -1,24 +1,42 @@
 //ts中引入的写法
 import * as React from "react";
-import { Button, Icon } from 'antd';
-const IconFont = Icon.createFromIconfontCN({
-    scriptUrl: '//at.alicdn.com/t/font_1638575_i23xlb5oazk.js',
-});
+import ToolMenu from "../Tool/ToolMenu"
+import eventSet from "../Tool/EventSet/eventset"
 
-export default class Sidebar extends React.Component{
+export interface Props{
+    functionMap:{[type:string]:Function}//接受传过来的函数字典
+}
+export interface State{
+    toolFunction:Function
+    eventSet:eventSet//自己封装的事件集合
+}
+
+//此处应该有个数据处理层
+export default class Sidebar extends React.Component<Props,State>{
+    constructor(props:Readonly<Props>){
+        super(props);
+    }
+    readonly state:State={
+        toolFunction:null,
+        eventSet:new eventSet(this)
+    }
+    //eventSet中的此函数到后期可以替换成 函数map，以处理多个值的更新
+    updateSomething(eventSet:eventSet){
+        if(this.state.toolFunction!=null)
+        this.state.toolFunction(eventSet)
+    }
+    //toolHandle函数传入ToolMenu组件中，以便设置工具类型以及该工具所用的函数
+    toolHandle(type:string){
+        let f=this.props.functionMap[type]
+        this.setState({
+            toolFunction:f
+        })
+    }
+    //调用toolFunction
     render(){
+        //bind之后，函数的作用域就是父组件的作用域了
         return (
-            <div>
-                <Button type="primary" shape="circle"><IconFont type="icon-pen" /></Button>
-                <Button type="primary" shape="circle"><IconFont type="icon-Line" /></Button>
-                <Button type="primary" shape="circle"><IconFont type="icon-Select-1" /></Button>
-                <Button type="primary" shape="circle"><IconFont type="icon-Select-" /></Button>
-                <Button type="primary" shape="circle"><IconFont type="icon-hand" /></Button>
-                <Button type="primary" shape="circle"><IconFont type="icon-Text-box" /></Button>
-                <Button type="primary" shape="circle"><IconFont type="icon-zoom" /></Button>
-                <Button type="primary" shape="circle"><IconFont type="icon-hand" /></Button>
-                <Button type="primary" shape="circle"><IconFont type="icon-Link-Select" /></Button>
-            </div>
+            <ToolMenu toolHandle={this.toolHandle.bind(this)}></ToolMenu>
         );
     };
 }
