51d7b9f59088060d9bcf3c5fb27055a8000e1c44
LYM <945002585@qq.com>
2020-02-28 16:25:32 +0800

    添加了缩放和平移功能

--- src/MyCanvas/MyCanvas.tsx
+++ src/MyCanvas/MyCanvas.tsx
@@ -6,18 +6,28 @@ const { Content } = Layout;
 
 
 const MyCanvas=()=>{
-    let myCanvas:HTMLCanvasElement = null;
-
+    let MyCanvas:HTMLCanvasElement = null;
     useEffect(() => {
         Paper.install(window);
-        Paper.setup(myCanvas)
+        Paper.setup(MyCanvas);
+        //使用React提供的onWheel会提示“渲染过多”    
+        //下面这段代码是实现缩放功能的初始化
+        MyCanvas.onwheel=(event)=>{
+            let v=Paper.view
+            if(event.deltaY<0){
+                v.scale(1.05,new Paper.Point(event.offsetX,event.offsetY))
+            }else{
+                v.scale(0.95,new Paper.Point(event.offsetX,event.offsetY))
+            }
+        }
     });
     return (
         <Content className="me-canvas">
           <canvas 
-          ref={ref=>{myCanvas=ref}}
+          ref={ref=>{MyCanvas=ref}}
           id="myCanvas"></canvas>
         </Content>
     )
 }
+
 export default MyCanvas
--- src/Tool/ToolMenu.tsx
+++ src/Tool/ToolMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as paper from "paper"
 import { Button , Icon ,Layout} from 'antd';
 const IconFont = Icon.createFromIconfontCN({
@@ -7,11 +7,19 @@ const IconFont = Icon.createFromIconfontCN({
 const {Sider}=Layout;
 
 const ToolMenu=()=>{
+
+    //工具映射函数
+    const FunctionMap=(ToolType:string)=>{
+        switch(ToolType){
+            case 'circle': return DrawTest;break;
+            case 'translate': return TranslateTest;break;
+        }
+    }
     return (
         <Sider width={48} className="me-left-bar">
             <div>
-                <Button type="primary" shape="circle" onClick={DrawTest}><IconFont type="icon-pen" /></Button>
-                <Button type="primary" shape="circle"><IconFont type="icon-Line" /></Button>
+                <Button type="primary" shape="circle" onClick={FunctionMap("circle")}><IconFont type="icon-pen" /></Button>
+                <Button type="primary" shape="circle" onClick={FunctionMap("translate")}><IconFont type="icon-Line" /></Button>
                 <Button type="primary" shape="circle"><IconFont type="icon-Select-1" /></Button>
                 <Button type="primary" shape="circle"><IconFont type="icon-Select-" /></Button>
                 <Button type="primary" shape="circle"><IconFont type="icon-hand" /></Button>
@@ -23,7 +31,26 @@ const ToolMenu=()=>{
     )
 }
 
+//拖拽功能
+const TranslateTest=()=>{
+    //清除所有的工具
+    paper.tools.forEach((item)=>{
+        item.remove()
+    })
+
+    let tool:paper.Tool=new paper.Tool()
+    let view:paper.View=paper.view;
+    tool.onMouseDrag=(event:paper.ToolEvent)=>{
+        view.translate(event.point.subtract(event.downPoint))
+    }
+}
+//画圆功能
 const DrawTest=()=>{
+    //清除所有的工具
+    paper.tools.forEach((item)=>{
+        item.remove()
+    })
+
     let tool:paper.Tool=new paper.Tool()
     tool.onMouseDrag=(event:paper.ToolEvent)=>{
         let path:paper.Path.Circle=new paper.Path.Circle({
