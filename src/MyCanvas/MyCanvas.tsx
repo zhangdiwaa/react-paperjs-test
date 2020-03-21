c67fcf10c2c9aeb14cb3fac514e9526e3b0d98c4
zhangdi <zhangdiwaa@163.com>
2020-03-21 18:52:19 +0800

    解决了paper.js canvas缩放问题，并且处理了主画布的滚动条问题，概览视图那里也可以模仿这里的做法。 此外更新了readme.md

--- src/MyCanvas/MyCanvas.tsx
+++ src/MyCanvas/MyCanvas.tsx
@@ -1,22 +1,16 @@
 import React from "react"
 import * as Paper from "paper";
-import {Button, Layout, Tooltip} from 'antd';
-import {useEffect} from "react";
-import EventHub from "../Common/Observer";
-import {PageChangeBefore, PageChangeAfter} from "../Common/UndoAndRedo"
-import {ClearSelected} from './PaperTools'
-import {createFromIconfontCN} from '@ant-design/icons';
+import { Button, Layout, Tooltip } from 'antd';
+import { useEffect, useState } from "react";
+import { createFromIconfontCN } from '@ant-design/icons';
 import Config from "../Common/Config";
-import {Refresh} from "../UI/Layers";
-import {RefreshOverview} from "../UI/Overview";
 import {
     ToolZoomauto,
     ToolZoomin,
     ToolZoomout
 } from "./PaperTools";
 
-const {Content} = Layout;
-const {Header} = Layout;
+const { Content } = Layout;
 const IconFont = createFromIconfontCN({
     scriptUrl: Config.IconUrl,
 });
@@ -46,20 +40,36 @@ const MyCanvas = () => {
         <Content className="me-canvas under-bottonbox">
             <div className="under-botton">
                 <Tooltip placement="bottom" title={"zoomin"}>
-                    <Button onClick={ToolZoomin}><IconFont type="icon-zoomin"/></Button>
+                    <Button onClick={ToolZoomin}><IconFont type="icon-zoomin" /></Button>
                 </Tooltip>
                 <Tooltip placement="bottom" title={"zoomout"}>
-                    <Button onClick={ToolZoomout}><IconFont type="icon-zoomout"/></Button>
+                    <Button onClick={ToolZoomout}><IconFont type="icon-zoomout" /></Button>
                 </Tooltip>
                 <Tooltip placement="bottom" title={"zoomauto"}>
-                    <Button onClick={ToolZoomauto}><IconFont type="icon-zoom1"/></Button>
+                    <Button onClick={ToolZoomauto}><IconFont type="icon-zoom1" /></Button>
                 </Tooltip>
             </div>
             <canvas
                 ref={ref => {
                     MyCanvas = ref
                 }}
-                id="myCanvas"></canvas>
+                /** 解决paper.js canvas缩放的办法
+                 * CSS
+                canvas[resize] {
+                    width: 100%;
+                    height: 100%;
+                }
+                * react jsx
+                <canvas
+                    ref={(el) => { this.canvas = el; }}
+                    data-paper-resize="true"
+                    data-paper-keepalive="true"
+                />
+                **/
+                data-paper-resize="true"
+                data-paper-keepalive="true"
+                style={{ width: '100%', height: '100%' }}
+                ></canvas>
 
         </Content>
 
